b31d2210304d5c3acee56190fd1baedb33ebec0c
Irish Micole Cando <irishmicolecando@gmail.com>
2022-11-19 11:07:25 +0800

    Added listening to the source and converting the audio to text in the main function

--- speech_to_text.py
+++ speech_to_text.py
@@ -8,3 +8,10 @@ def main():
     with speech_recog.Microphone as source:
         recognizer.adjust_for_ambient_noise(source)
         print("Say something...")
+
+        # listening to the source
+        audio = recognizer.listen(source)
+
+        # converting the audio to text file
+        try:
+            print("You said: ")
