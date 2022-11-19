34430472d216789cea3131cbbeed9fb80b4bab58
Irish Micole Cando <irishmicolecando@gmail.com>
2022-11-19 11:05:05 +0800

    Defined a function that sets the source of the audio

--- speech_to_text.py
+++ speech_to_text.py
@@ -1,2 +1,10 @@
 # importing needed packages
 import speech_recognition as speech_recog
+
+def main():
+    # initialize the recognizer
+    recognizer = speech_recog.Recognizer()
+    # source of audio - microphone
+    with speech_recog.Microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        print("Say something...")
